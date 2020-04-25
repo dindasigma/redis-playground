34b82eaf2657ec145abfd4ef886642254dc7a007
dindasigma <dinda.sgmwty@gmail.com>
2020-04-25 12:10:07 +0700

    add basic pub sub

--- main.go
+++ main.go
@@ -12,4 +12,5 @@ func main() {
 	fmt.Println("Go Redis Playground")
 	client.Init()
 	client.Basic()
+	client.Pubsub()
 }
---
+++ packages/pubsub.go
@@ -0,0 +1,37 @@
+package playground
+
+import (
+	"fmt"
+	"time"
+)
+
+func (server *Server) Pubsub() {
+	pubsub := server.RDB.Subscribe("mychannel1")
+
+	// Wait for confirmation that subscription is created before publishing anything.
+	_, err := pubsub.Receive()
+	if err != nil {
+		panic(err)
+	}
+
+	// Go channel which receives messages.
+	ch := pubsub.Channel()
+
+	// Publish a message.
+	err = server.RDB.Publish("mychannel1", "hello").Err()
+	if err != nil {
+		panic(err)
+	}
+
+	time.AfterFunc(time.Second, func() {
+		// When pubsub is closed channel is closed too.
+		_ = pubsub.Close()
+	})
+
+	// Consume messages.
+	for msg := range ch {
+		fmt.Println(msg.Channel, msg.Payload)
+	}
+
+	// Output: mychannel1 hello
+}
