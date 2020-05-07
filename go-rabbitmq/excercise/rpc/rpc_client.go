db3595479373064134f35d30e32da3d7882d72de
dindasigma <dinda.sgmwty@gmail.com>
2020-05-07 15:27:40 +0700

    [go-rabbitmq] add rpc excercise

---
+++ go-rabbitmq/excercise/rpc/rpc_client.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/streadway/amqp"
+	"github.com/dindasigma/go-rabbitmq/excercise/utils"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := utils.BodyFrom(os.Args)
+	i, _ := strconv.Atoi(n)
+
+	log.Printf(" [x] Requesting fib(%d)", n)
+	res, err := fibonacciRPC(i)
+	utils.FailOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %d", res)
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func fibonacciRPC(n int) (res int, err error) {
+	conn, ch := utils.GetChannel()
+	defer conn.Close()
+	defer ch.Close()
+
+	q := utils.GetQueue("", false, false, true, false,nil, ch)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"", // consumer
+		true, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil, // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	corrId := randomString(32)
+
+	err = ch.Publish(
+		"", // exchange
+		"rpc_queue", // routing key
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			CorrelationId: corrId,
+			ReplyTo: q.Name,
+			Body: []byte(strconv.Itoa(n)),
+		})
+	utils.FailOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res, err = strconv.Atoi(string(d.Body))
+			utils.FailOnError(err, "Failed to convert body to integer")
+			break
+		}
+	}
+
+	return
+}
---
+++ go-rabbitmq/excercise/rpc/rpc_server.go
@@ -0,0 +1,77 @@
+package main
+
+// go run rpc_server.go
+// go run rpc_client.go 30
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/streadway/amqp"
+	"github.com/dindasigma/go-rabbitmq/excercise/utils"
+)
+
+func main() {
+	conn, ch := utils.GetChannel()
+	defer conn.Close()
+	defer ch.Close()
+
+	q := utils.GetQueue("rpc_queue", false, false, true, false,nil, ch)
+
+	err := ch.Qos(
+		1, // prefetch count
+		0, // prefetch size
+		false, // global
+	)
+	utils.FailOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"", // consumer
+		false, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil, // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			n, err := strconv.Atoi(string(d.Body))
+			utils.FailOnError(err, "Failed to convert body to integer")
+
+			log.Printf(" [.] fib(%d)", n)
+			response := fib(n)
+
+			err = ch.Publish(
+				"", // exchange
+				d.ReplyTo, // routing key
+				false, // mandatory
+				false, // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body: []byte(strconv.Itoa(response)),
+				})
+			utils.FailOnError(err, "Failed to publish a message")
+
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf(" [*] Awaiting RPC requests")
+	<-forever
+}
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fib(n-1) + fib(n-2)
+	}
+}
