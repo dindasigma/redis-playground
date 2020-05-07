0e54a2040c94e2724617bf202037aef290fb17e2
dindasigma <dinda.sgmwty@gmail.com>
2020-05-07 12:12:51 +0700

    [go-rabbitmq] update queues code documentation

--- go-rabbitmq/excercise/queues/new_task.go
+++ go-rabbitmq/excercise/queues/new_task.go
@@ -1,6 +1,11 @@
 package main
 
-// avoid doing a resource-intensive task immediately and having to wait for it to complete
+// tasks will survive even if RabbitMQ is restarted.
+
+// add d.Ack(false) (this acknowledges a single delivery), once we're done with a task.
+// set durable = true on QueueDeclare on both producer and consumer
+// mark our messages as persistent - by using the amqp.Persistent option amqp.Publishing takes
+// set the prefetch count on worker with the value of 1. This tells RabbitMQ not to give more than one message to a worker at a time. Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one. Instead, it will dispatch it to the next worker that is not still busy.
 
 import (
 	"github.com/dindasigma/go-rabbitmq/excercise/utils"
