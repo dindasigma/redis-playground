b7f76c27b4a48fc9deff1c79f027dee5825c7a7f
dindasigma <dinda.sgmwty@gmail.com>
2020-05-07 12:50:32 +0700

    [go-rabbitmq] add pubsub excercise

---
+++ go-rabbitmq/excercise/pubsub/emit_log.go
@@ -0,0 +1,50 @@
+package main
+
+// deliver a message to multiple consumers
+// using own declared exchanges: receives messages from producers and the other side it pushes them to queues
+// exchange type: fanout (broadcasts all the messages it receives to all the queues it knows)
+// by default exchange type is direct (guarantee that only one of client get the message and only one time)
+
+// so we will publish to exchange not queue
+
+import (
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+	"github.com/dindasigma/go-rabbitmq/excercise/utils"
+)
+
+func main() {
+	conn, ch := utils.GetChannel()
+	defer conn.Close()
+	defer ch.Close()
+
+	err := ch.ExchangeDeclare(
+		"logs", // name
+		"fanout", // type
+		true, // durable
+		false, // auto-deleted
+		false, // internal
+		false, // no-wait
+		nil, // arguments
+	)
+	utils.FailOnError(err, "Failed to declare an exchange")
+
+	body := utils.BodyFrom(os.Args)
+
+	// publish with our logs exchange
+	err = ch.Publish(
+		"logs", // exchange
+		"",     // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	utils.FailOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+}
+
---
+++ go-rabbitmq/excercise/pubsub/receive_logs.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dindasigma/go-rabbitmq/excercise/utils"
+)
+
+func main() {
+	conn, ch := utils.GetChannel()
+	defer conn.Close()
+	defer ch.Close()
+
+	err := ch.ExchangeDeclare(
+		"logs", // name
+		"fanout", // type
+		true, // durable
+		false, // auto-deleted
+		false, // internal
+		false, // no-wait
+		nil, // arguments
+	)
+	utils.FailOnError(err, "Failed to declare an exchange")
+
+	// create a queue with a random name (let the server choose a random queue name for us)
+	// and when the connection that declared it closes, the queue will be deleted because it is declared as exclusive.
+	q := utils.GetQueue("", false, false, true, false,nil, ch)
+
+	// bind queue and exchange, tell the exchange to send messages to our queue
+	err = ch.QueueBind(
+		q.Name, // queue name
+		"", // routing key
+		"logs", // exchange
+		false,
+		nil,
+	)
+	utils.FailOnError(err, "Failed to bind a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"", // consumer
+		true, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil, // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+}
